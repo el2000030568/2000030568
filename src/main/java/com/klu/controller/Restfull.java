ef7f2b002e20fcaa629aa865a7b7adb7ca70bf61
el2000030568 <107025191+el2000030568@users.noreply.github.com>
2023-06-12 16:24:41 +0530

    Update Restfull.java

--- src/main/java/com/klu/controller/Restfull.java
+++ src/main/java/com/klu/controller/Restfull.java
@@ -1,17 +1,34 @@
 package com.klu.controller;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController; 
 
 @RestController
 @RequestMapping("/train")
 public class Restfull {
-         
+    
+	private static int TrainNumber;
+	private static String TrainName;
 	
-		
+	@GetMapping("/fill/{number}/{name}")
+	public void fill(@PathVariable("number") int num,@PathVariable("name") String na)
+	{
+		TrainNumber = num;
+		TrainName = na;
 	}
+	
+	@GetMapping("/getrequest/TrainNumber")
+	public int Getrequest() 
+	{
+		return TrainNumber;
+	}
+	
+	@GetMapping("/getrequest/TrainName")
+	public String Getrequest1() 
+	{
+		return TrainName;
+	}
+		
+}
 	
